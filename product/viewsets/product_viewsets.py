039ed223ee4393980f9fb652ef49b9eb756826f1
Erick <ericklemos2001@hotmail.com>
2026-02-20 11:02:44 -0300

    add autenticação rest framework e correções de teste

--- product/viewsets/product_viewsets.py
+++ product/viewsets/product_viewsets.py
@@ -1,6 +1,9 @@
-from rest_framework import status
-from rest_framework.mixins import CreateModelMixin
-from rest_framework.response import Response
+from rest_framework.authentication import (
+    SessionAuthentication,
+    BasicAuthentication,
+    TokenAuthentication,
+)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
@@ -8,7 +11,13 @@ from product.serializers.product_serializers import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
+    authentication_classes = [
+        SessionAuthentication,
+        BasicAuthentication,
+        TokenAuthentication,
+    ]
+    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
-        return Product.objects.all()
+        return Product.objects.all().order_by("id")
